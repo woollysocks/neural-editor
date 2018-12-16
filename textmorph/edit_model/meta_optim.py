87bb355a7cbae9ed79db21b001a0a2b8c59c0539
woollysocks <nikitanangia@gmail.com>
2018-12-15 20:39:53 -0500

    gpu attempt. not cpu runnable

--- textmorph/edit_model/meta_optim.py
+++ textmorph/edit_model/meta_optim.py
@@ -9,6 +9,7 @@ import random
 import shutil
 
 import torch
+from torch import cuda
 from gtd.ml.torch.utils import GPUVariable
 from torch.autograd import Variable
 import torch.nn as nn
@@ -80,7 +81,7 @@ class OptimN2N:
             for p in self.param_grads:
                 p.zero_()
         for k in range(self.iters):
-            self.all_z.append(GPUVariable(torch.FloatTensor(input[0].size()).normal_(0, 1))) # , requires_grad=True
+            self.all_z.append(Variable(torch.cuda.FloatTensor(input[0].size()).normal_(0, 1))) # , requires_grad=True
             torch.manual_seed(int(self.seeds[k]))
             mean_svi, logvar_svi = input
             z_samples = self.encoder._reparameterize(mean_svi, logvar_svi, self.all_z[k])
